e18b4bea39a8ef8eec736e89796c20968348e9b2
gabriel12QK <jose.rivas@espam.edu.ec>
2022-12-07 11:09:06 -0500

    api registro user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    public $timestamps = false;
     protected $fillable = [
         'name',
         'last_name',
