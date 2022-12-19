d6cd1bc90fa38b87872a7088620372faef0b639b
andres174 <zambrasda17@gmail.com>
2022-12-19 16:13:20 -0500

    repartidor y orden de rutas

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,35 @@ use App\Http\Controllers\OrdenController;
 |
 */
 
+//usuario
 Route::post('/registro', [UserController::class, 'register']);
 Route::post('/update-email/{id}', [UserController::class, 'editUserEmail']);
 Route::post('/update-password/{id}', [UserController::class, 'editPassword']);
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/user-index', [UserController::class, 'index']);
 Route::get('/show/{id}', [UserController::class, 'Show']);
+//repartidor
+Route::post('/registro-repartidor', [UserController::class, 'registerRepartidor']);
+
+// orden - pedido
 Route::get('/show-orden-estado', [OrdenController::class, 'ShowOrdenEstado']);
+
+//producto
 Route::resource('producto', ProductoController::class);
-Route::resource('categoria', CategoriaController::class);
-Route::resource('promocion-producto',PromocionProductoController::class);
-Route::resource('marca', MarcaController::class);
 Route::post('/edit-precio/{id}',[ ProductoController::class,'editPrecio']);
 Route::post('/edit-stock/{id}',[ ProductoController::class,'editStock']);
 Route::post('/edit-imagen/{id}',[ ProductoController::class,'editImagen']);
+
+//categoria
+Route::resource('categoria', CategoriaController::class);
+
+//promocion producto
+Route::resource('promocion-producto',PromocionProductoController::class);
+
+//marca
+Route::resource('marca', MarcaController::class);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
