faab64090e529aeaf50091b47a446462b8e80ad3
andres174 <zambrasda17@gmail.com>
2022-12-19 11:06:25 -0500

    cosas usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     public function register(Request $request)
     {
        // return response()->json($request);
-         $validData = $request->validate([
+        $validData = $request->validate([
         'name' => 'required|string|max:255',
         'last_name' => 'required|string|max:255',
         'referencia'=>'required',
@@ -129,6 +129,47 @@ class UserController extends Controller
             return response()->json(['message' => 'usuario no encontrado'], 404);
         }
         return response()->json($user);
+    }
+
+    public function registerRepartidor(Request $request){
+
+        $validData = $request->validate([
+            'name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'referencia'=>'required',
+            'direccion'=>'required|string|max:255',
+            'telefono'=>'required|string',
+            'cedula'=>'required|string|max:10',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'imagen'=>'required'
+            ]);
+
+            $type=2;
+
+            $user = User::create([
+            'name' => $validData['name'],
+            'last_name' => $validData['last_name'],
+            'email' => $validData['email'],
+            'referencia' => $validData['referencia'],
+            'direccion' => $validData['direccion'],
+            'telefono' => $validData['telefono'],
+            'cedula' => $validData['cedula'],
+            'password' => Hash::make($validData['password']),
+            'imagen' => $validData['imagen'],
+            'estado' => 1,
+            'id_tipo_usuario' => $type,
+            ]);
+    
+         //imagen
+             $img=$request->file('imagen');
+            $validData['imagen'] = time().'.'.$img->getClientOriginalExtension();
+    
+     
+             $request->file('imagen')->storeAs("public/images/persona/{$user->id}", $validData['imagen']);
+    
+             return response()->json(['message' => 'Usuario registrado'], 200);
+
     }
 
    
