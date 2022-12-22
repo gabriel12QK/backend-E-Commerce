72b897f175567bd34443a3d512a6ecf2eb78fa07
andres174 <zambrasda17@gmail.com>
2022-12-22 08:32:50 -0500

    favicon y home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>App Delivery</title>
+    <title>APIS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Apis App Delivery</h1>
-    <p>prueba ramas 3 master</p>
-    <p>Prueba Bermeo</p>
-    <p>Prueba Git conection A</p>
+    <div class="container">
+        <div class="d-flex justify-content-center" style="margin-top: 10rem">
+            <div class="card" style="width: 30rem;" style="margin: auto;">
+                <div class="card-header text-center">
+                    DeligoApp APIS
+                </div>
+                <img src="https://drive.google.com/uc?export=download&id=1Msdnpcm2o1PPtiVB5kHj_QglLK4nvjFT" class="card-img-top" alt="...">
+                <div class="card-body">
+                <p class="card-text">Desarrollado por Sexto Semestre. Para la asignatura de Aplicaciones para Dispositivos Móviles.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
