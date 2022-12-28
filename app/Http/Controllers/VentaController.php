5cf019f4996a9068612b5ecbf6457985a73ddfa5
gabriel12QK <jose.rivas@espam.edu.ec>
2022-12-28 17:58:14 -0500

    correcion en consulta

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\venta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Collection;
 
 class VentaController extends Controller
 {
@@ -86,16 +89,56 @@ class VentaController extends Controller
 
     public function ShowVenta()
     {
+        $dataVenta=Array();
+        $dataProductos=Array();
+        $datakit=Array();
         $venta= DB::table('ventas')
-        ->join('id_venta','ventas.detalle_ventas','=','detalle_venta.id')
-        ->join('user','ventas.id_user', '=','users.id')
-        ->join('tipo_pagos','ventas.id_tipo_pago','=','tipo_pago.id')
-        ->select('ventas.*',
-        'users.name','users.last_name','users.cedula',
-        'detalle_ventas.precio','detalle_ventas.cantidad',
+        ->join('detalle_ventas','ventas.id','=','detalle_ventas.id_venta')
+        ->join('users','ventas.id_user', '=','users.id')
+        ->join('tipo_pagos','ventas.id_tipo_pago','=','tipo_pagos.id')
+        ->select('ventas.id',
+        'users.name','users.last_name','users.cedula','users.id as userId',
+        'detalle_ventas.precio','detalle_ventas.cantidad','detalle_ventas.id_producto','detalle_ventas.id_promocion_producto','detalle_ventas.id_registro_promocion',
         'tipo_pagos.descripcion as tipo_pago'
         )
         ->where('ventas.estado',1)
         ->get();
+        
+       foreach ($venta as $key => $value) {
+        if ($value->id_producto!=null) {
+                       $detalleVentaProducto=DB::table('detalle_ventas')
+                       ->join('ventas','detalle_ventas.id_venta','=','ventas.id')
+                        ->join('productos','detalle_ventas.id_producto','=','productos.id')
+                        ->select('productos.nombre', 'productos.precio','detalle_ventas.cantidad')
+                        ->where('detalle_ventas.id_venta',$value->id)
+                        ->where('ventas.id_user',$value->userId)
+                        ->get();
+                       array_push($dataVenta,['nombreComprador:'=>$value->name,'apellidoComprador'=>$value->last_name, 'productos'=>$detalleVentaProducto,]);
+                            } 
+        else if ($value->id_registro_promocion!=null) {
+                 $detalleVentaKit=DB::table('detalle_ventas')
+                 ->join('registro_promocions','detalle_ventas.id_registro_promocion','=','registro_promocions.id')
+                  ->join('precio_kits','registro_promocions.id','=','precio_kits.id_registro_promocion')
+                  ->join('tipo_promocions','registro_promocions.id_tipo_promocion','=','tipo_promocions.id')
+                  ->join('kits','registro_promocions.id','=','kits.id')
+                  ->select('registro_promocions.*','tipo_promocions.descripcion as tipoDescripcion','kits.id as kitId','precio_kits.precio as precioKit')
+                 ->where('registro_promocions.estado',1)
+                // ->where('registro_promocions.id',$id)
+                  ->get();
+                  $kits=DB::table('kits')
+                    ->join('productos','kits.id_producto','=','productos.id')
+                    ->join('tipo_pesos','productos.id_tipo_peso','=','tipo_pesos.id')
+                    ->select('kits.cantidad','productos.nombre','productos.peso','tipo_pesos.descripcion')
+                    ->where('id_registro_promocion',$value->id_registro_promocion)
+                    ->get();
+                    foreach ($detalleVentaKit as $key => $value1) {
+                        array_push($datakit,['venta'=>$value1->descripcion,'tipoPromocion'=>$value1->tipoDescripcion,'precioKit'=>$value1->precioKit,'cantidadRestante'=>$value1->cantidad_restante,'contenidoKit'=>$kits]);
+                    }
+                    array_push($dataVenta,['nombreComprador:'=>$value->name,'apellidoComprador'=>$value->last_name,'kits'=>$datakit,]);
+                   
+        }
+       }
+
+        return response()->json($dataVenta);
     }
 }
